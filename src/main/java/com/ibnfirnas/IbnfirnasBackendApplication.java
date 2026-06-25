5cac7c605d2946023a002ee2ff1af16fe0a00e1d
Mohd Anwar <mohdanwarwind@gmail.com>
2026-06-25 21:40:22 +0530

    Initial commit:IbnFirnas Backend

---
+++ src/main/java/com/ibnfirnas/IbnfirnasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ibnfirnas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbnfirnasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbnfirnasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ibnfirnas/IbnfirnasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibnfirnas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbnfirnasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
