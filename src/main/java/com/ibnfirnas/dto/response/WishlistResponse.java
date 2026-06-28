cd9f1bd2d36a3d391f9bb1d3a19084ccaa9c8e16
aliya neda <137129762+aliyaneda0@users.noreply.github.com>
2026-06-29 02:58:52 +0530

    add wishlist response in dto

--- src/main/java/com/ibnfirnas/dto/response/WishlistResponse.java
+++ src/main/java/com/ibnfirnas/dto/response/WishlistResponse.java
@@ -1,17 +1,18 @@
 package com.ibnfirnas.dto.response;
 
+import lombok.Builder;
+import lombok.Getter;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-public record WishlistResponse(
-        Long id,
-        Long productId,
-        String productName,
-        String productImageUrl,
-        BigDecimal productPrice,
-        LocalDateTime addedAt
-) {
-
-
+@Getter
+@Builder
+public class WishlistResponse {
+    private Long id;
+    private Long productId;
+    private String productName;
+    private String productImageUrl;
+    private BigDecimal productPrice;
+    private LocalDateTime addedAt;
 }
