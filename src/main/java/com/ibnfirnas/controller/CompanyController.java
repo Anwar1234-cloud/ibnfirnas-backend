36e52bf203d8a8215db41830958e406546d2079e
Mohd Anwar <mohdanwarwind@gmail.com>
2026-07-06 01:33:37 +0530

    Changes in CompanyController and GalleryController

--- src/main/java/com/ibnfirnas/controller/CompanyController.java
+++ src/main/java/com/ibnfirnas/controller/CompanyController.java
@@ -2,6 +2,7 @@ package com.ibnfirnas.controller;
 
 import com.ibnfirnas.dto.response.ApiResponse;
 import com.ibnfirnas.entity.Company;
+import com.ibnfirnas.repository.CompanyRepository;
 import com.ibnfirnas.service.CompanyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,10 +14,26 @@ import org.springframework.web.bind.annotation.*;
 public class CompanyController {
 
     private final CompanyService companyService;
+    private final CompanyRepository companyRepository;
 
     @GetMapping
     public ResponseEntity<ApiResponse<Company>> getCompanyInfo() {
         return ResponseEntity.ok(
                 ApiResponse.success("Company info fetched", companyService.getCompanyInfo()));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<Company>> update(
+            @PathVariable Long id,
+            @RequestBody Company company) {
+        company.setId(id);
+        return ResponseEntity.ok(ApiResponse.success("Company updated",
+                companyRepository.save(company)));
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<Company>> create(
+            @RequestBody Company company) {
+        return ResponseEntity.ok(ApiResponse.success("Company created",
+                companyRepository.save(company)));
     }
 }
